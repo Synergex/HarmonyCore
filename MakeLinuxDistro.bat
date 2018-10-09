550b3e58056475c30595991aee7c0c8dcbe2b103
Steve Ives <steve@steveives.com>
2018-10-09 16:41:23 -0500

    Renamed postman export file.

--- MakeLinuxDistro.bat
+++ MakeLinuxDistro.bat
@@ -3,13 +3,13 @@ setlocal
 
 set PATH=C:\Program Files\7-Zip;%PATH%
 
-set SolutionDir=%~dp0
+set SolutionDir=%~dp0\
 pushd %SolutionDir%
 
 if not exist distribution\linux\debug\.   mkdir distribution\linux\debug
 if not exist distribution\linux\release\. mkdir distribution\linux\release
 
-pushd Services.Test
+pushd Services.Host
 echo Building debug distribution
 dotnet publish -c Debug   -r linux-x64 -o ..\distribution\linux\debug
 echo Building release distribution
