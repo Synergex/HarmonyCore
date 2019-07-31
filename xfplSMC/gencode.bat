4cfb6afa6ffcd22abbb15a0ee7bc9fb901ea6c41
Johnson Luong <johnson.luong@synergex.com>
2019-07-31 16:59:14 -0700

    Updated traditional bridge batch file to use smcstrs. Template changes to allow building again.

--- xfplSMC/gencode.bat
+++ xfplSMC/gencode.bat
@@ -1,7 +1,20 @@
 @echo off
 
 codegen -smc %SMCROOT%\%SMCNAME% -interface %TESTNAME% -t %SolutionDir%\%TEMPLATEROOT%\InterfaceDispatcher -o %SolutionDir%\%PROJECT%\Dispatcher -n %NAMESPACE% -ut MODELS_NAMESPACE=%NAMESPACE%.Models -e -r -lf
+if ERRORLEVEL 1 goto error
 
 codegen -smc %SMCROOT%\%SMCNAME% -interface %TESTNAME% -t %SolutionDir%\%TEMPLATEROOT%\InterfaceMethodDispatchers -o %SolutionDir%\%PROJECT%\MethodDispatchers -n %NAMESPACE% -ut MODELS_NAMESPACE=%NAMESPACE%.Models -e -r -lf
+if ERRORLEVEL 1 goto error
 
-codegen -s %DATA_STRUCTURES% -ms -t %SolutionDir%\%TEMPLATEROOT%\InterfaceDispatcherData -o %SolutionDir%\%PROJECT%\DispatcherData -n %NAMESPACE% -ut SMC_INTERFACE=%TESTNAME% -e -r -lf
+codegen -smcstrs %SMCROOT%\%SMCNAME% -ms -t %SolutionDir%\%TEMPLATEROOT%\InterfaceDispatcherData -o %SolutionDir%\%PROJECT%\DispatcherData -n %NAMESPACE% -ut SMC_INTERFACE=%TESTNAME% -e -r -lf
+if ERRORLEVEL 1 goto bypass
+
+goto done
+
+:bypass
+exit /b 0
+
+:error
+echo *** CODE GENERATION INCOMPLETE ***
+
+:done
