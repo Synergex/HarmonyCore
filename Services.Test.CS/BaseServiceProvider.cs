0cdfd0f1819ec3420c3f53ea195a07a66f8fdf59
Jeffrey Greene <the_elhefe@outlook.com>
2021-03-11 09:05:50 -0800

    added missing file

---
+++ Services.Test.CS/BaseServiceProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Test.CS
+{
+    class BaseServiceProvider
+    {
+        static BaseServiceProvider()
+        {
+            var startupClass = new Startup(null, null);
+            var startupServices = new ServiceCollection();
+            startupClass.ConfigureServices(startupServices);
+            provider = startupServices.BuildServiceProvider();
+        }
+        private static IServiceProvider provider;
+        public static IServiceScope Services
+        {
+            get
+            {
+                return provider.CreateScope();
+            }
+        }
+
+        public static void Cleanup()
+        {
+            (provider as IDisposable)?.Dispose();
+            provider = null;
+        }
+    }
+}
