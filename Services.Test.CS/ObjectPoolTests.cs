0228c401a378a7261a8e8f7d4b7aa33d126abbfe
Jeffrey Greene <the_elhefe@outlook.com>
2021-08-19 12:22:41 -0700

    replaced implementation for object pool and added concurrency tester

---
+++ Services.Test.CS/ObjectPoolTests.cs
@@ -0,0 +1,76 @@
+﻿using Harmony.Core.Context;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Services.Test.CS
+{
+    [TestClass]
+    public class ObjectPoolTests
+    {
+        class MyTestContext : IPooledContextBase2, IContextBase
+        {
+            [ThreadStatic]
+            static Random _random;
+            internal static int _instanceCount;
+            bool _inited = false;
+            public bool IsHealthy => _random.Next(0, 2) == 0;
+
+            public ContextIsolationLevel IsolationLevel => ContextIsolationLevel.FreeThreaded;
+
+            public MyTestContext()
+            {
+                Interlocked.Increment(ref _instanceCount);
+            }
+
+            public void Destroy()
+            {
+                _inited = false;
+                Interlocked.Decrement(ref _instanceCount);
+            }
+
+            public Task EnsureReady()
+            {
+                Assert.IsTrue(_inited);
+                return Task.FromResult(true);
+            }
+
+            public void InitServices(IServiceProvider sp)
+            {
+                _inited = true;
+            }
+
+            public Task Recycle()
+            {
+                _inited = false;
+                return Task.FromResult(true);
+            }
+        }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            var tasks = new List<Task>();
+            BlockingPoolContextFactory<MyTestContext> contextFactory = new BlockingPoolContextFactory<MyTestContext>((sp) => new MyTestContext(), 6, 4, TimeSpan.FromSeconds(30), true);
+            for(int i = 0; i < 8; i++)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    for (int ii = 0; ii < 1000; ii++)
+                    {
+                        var madeContext = contextFactory.MakeContext(null);
+                        madeContext.EnsureReady().Wait();
+                        contextFactory.ReturnContext(madeContext);
+                    }
+                }));
+            }
+            Task.WhenAll(tasks).Wait();
+            Assert.IsTrue(MyTestContext._instanceCount <= 4);
+            contextFactory.TrimPool(0).Wait();
+            Assert.AreEqual(MyTestContext._instanceCount, 0);
+        }
+    }
+}
