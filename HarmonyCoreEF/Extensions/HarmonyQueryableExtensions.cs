96a9d5d5247d52248a4d07ec5e565517f953a948
Jeffrey Greene <the_elhefe@outlook.com>
2021-10-25 12:53:48 -0700

    added new files

---
+++ HarmonyCoreEF/Extensions/HarmonyQueryableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmony.Core.EF.Extensions
+{
+    class HarmonyQueryableExtensions
+    {
+        internal static readonly MethodInfo LeftJoinMethodInfo = typeof(QueryableExtensions).GetTypeInfo().GetDeclaredMethods("LeftJoin").Single((MethodInfo mi) => mi.GetParameters().Length == 5);
+    }
+}
