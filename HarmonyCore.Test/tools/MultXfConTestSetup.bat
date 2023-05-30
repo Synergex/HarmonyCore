c976538f72209a880999655624dc0410884f60a0
Taras Yakovchuk <tarasyakovchuk@gmail.com>
2023-05-30 11:36:12 -0700

    Testing xfserver setup script

--- HarmonyCore.Test/tools/MultXfConTestSetup.bat
+++ HarmonyCore.Test/tools/MultXfConTestSetup.bat
@@ -18,11 +18,11 @@ start /min /wait rsynd -x -c TestSrv -p %2
 goto done 
 
 :setup
-start /min /wait rsynd -rs -n -c TestSrv -p %2
+start /wait rsynd -rs -n -c TestSrv -p %2
 rem add xfserver env var
-start /min /wait powershell Start-Process powershell -WindowStyle hidden -verb runAs -ArgumentList \"reg add 'HKEY_LOCAL_MACHINE\SOFTWARE\Synergex\Synergy xfServer\TestSrv\Synrc' /v %4 /d %3\"
-start /min /wait rsynd -q -c TestSrv -p %2
-start /min /wait rsynd -rs -n -c TestSrv -p %2
+start /wait powershell Start-Process powershell -WindowStyle hidden -verb runAs -ArgumentList \"reg add 'HKEY_LOCAL_MACHINE\SOFTWARE\Synergex\Synergy xfServer\TestSrv\Synrc' /v %4 /d %3\"
+start /wait rsynd -q -c TestSrv -p %2
+start /wait rsynd -rs -n -c TestSrv -p %2
 goto done
 
 :run
